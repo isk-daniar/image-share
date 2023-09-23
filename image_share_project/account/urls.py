1db27e0e1433538ae40c20277b6b9db5fc3d6a11
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-09-23 19:25:01 +0500

    исправлен ошибка при входе и выходе

--- image_share_project/account/urls.py
+++ image_share_project/account/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     # ulr входа и выхода
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='account/registration/logged_out.html'), name='logout'),
 
     path('', views.dashboard, name='dashboard')
 ]
